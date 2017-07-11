a1d4817f5e4865058c8e2b6d040f14a4ecc62aa0
Oct17th <736842589@qq.com>
2017-07-11 20:51:22 +0800

    Complete part of BinaryTreePath

--- src/main/java/alibaba/day_20170313/BinaryTreePath.java
+++ src/main/java/alibaba/day_20170313/BinaryTreePath.java
@@ -1,15 +1,29 @@
 package alibaba.day_20170313;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 /**
  * 17.3.13
  * 内推在线编程测试
- *
- 计算以每个结点为根节点的树的子节点数n*该节点v
- 有一个二叉树，记为三位数十进制数组，设深度不大于4
- 百位为深度L，十位为位置P，个位为值V，求各路径数值和
- 百位十位为递增数，百位0~4，十位0~8
+ * <p>
+ * 计算以每个结点为根节点的树的子节点数n*该节点v
+ * 有一个二叉树，记为三位数十进制数组。百位为深度L，十位为位置P，个位为值V，求给定节点全部子路径下的叶子节点数值(三位数LPV)的和
+ * 百位十位为递增数，百位1~4，十位0~8
+ * <p>
+ * 百位十位数值示意图： 2^(n-1) -1+currentNode_P
+ * <p>
+ * currentNode_L              currentNode_P
+ * <p>
+ * 1              0                                 10 0
+ * /    \
+ * 2         0       1                          20 1   21 2
+ * /  \    /  \
+ * 3      0   1   2   3                       30 3  31 4  32 5  33 6
+ * / \ / \ / \ / \
+ * 4     0 1 2 3 4 5 6 7                         40 7 |41 8 |42 9 |43 10 |44 11 |45 12 |46 13 |47 14
  *
  * @author YiJie
  * @date May 9, 2017
@@ -17,28 +31,164 @@ import java.io.File;
 public class BinaryTreePath {
 
 
+    private List<Node> tree;
+    private int currentNode_L;
+    private int currentNode_P;
+    private int treesize;
+    private List<Node> leaves;
+    private List<Node> markleaves;
+    private List<Integer> marks;
 
-}
+    public BinaryTreePath() {
+        tree = new ArrayList<Node>(16 - 1);//题目规定深度不大于4
+        currentNode_L = -1;
+        currentNode_P = -1;
+        treesize = tree.size();
+    }
 
-class Node{
-    int L;
-    int P;
-    int V;
-    int num;
-    Node(int num){
-        this.num=num;
-        this.L=num/100;
-        this.P=num%100/10;
-        this.V=num%10;
+    public BinaryTreePath(int[] n) {
+        this();
+        for (int i = 0; i < n.length; i++) {
+            addNode(n[i]);
+        }
     }
-    @Override
-    public String toString() {
-        return "Node [L=" + L + ", P=" + P + ", V=" + V + ", num=" + num + "]";
+
+    public void addNode(int V) {
+        if (currentNode_L == -1 && currentNode_P == -1) {
+            tree.add(new Node(currentNode_L = 0, currentNode_P = 0, V));
+            treesize++;
+            return;
+        }
+        int L, P;
+        int lastIndexoflineL = (int) Math.pow(2, this.currentNode_L)-1;
+        if (this.currentNode_P == lastIndexoflineL) {
+            L = ++this.currentNode_L;
+            P = this.currentNode_P = 0;
+        } else {
+            L = this.currentNode_L;
+            P = ++this.currentNode_P;
+        }
+        tree.add(new Node(L, P, V));
+        treesize++;
     }
-}
-class Tree{
-    int[] l = {0,1,2,3,4};
-    int[] p = {0,1,2,3,4,5,6,7,8,9};
-    Node n;
-//	Integer.toBinaryString();
+
+
+    private int getIndex(int L, int P) {
+        int temp = (int) (Math.pow(2, L));//前L-1行的节点个数
+        return temp - 1 + P;//加上改行的index
+    }
+
+    private int getLeft(int L, int P) {
+        return getIndex(L + 1, 2 * P);
+    }
+
+    private int getRight(int L, int P) {
+        return getIndex(L + 1, 2 * P + 1);
+    }
+
+
+    public List<Node> getLeaves(int L, int P) {
+        leaves = new ArrayList<Node>();
+        setLeaves(L, P);
+        return leaves.size() == 0 ? null : leaves;
+    }
+
+
+    private void markLeaves(int L,int P){
+        Integer maxindex = treesize - 1;
+        Integer leftindex0 = getLeft(L, P);
+        Integer leftindex = leftindex0 <= maxindex ? leftindex0 : null;
+        Integer rightindex0 = getRight(L, P);
+        Integer rightindex = rightindex0 <= maxindex ? rightindex0 : null;
+
+        if (leftindex == null && rightindex == null) {
+//            markleaves.get(getIndex(L, P)).markLeaf();
+            marks.add(getIndex(L, P));
+            return;
+        }
+        if (leftindex != null) {
+            markLeaves(L + 1, 2 * P);
+        }
+        if (rightindex != null) {
+            markLeaves(L + 1, 2 * P + 1);
+        }
+
+    }
+
+    private void setLeaves(int L, int P) {
+        Integer maxindex = treesize - 1;
+        Integer leftindex0 = getLeft(L, P);
+        Integer leftindex = leftindex0 <= maxindex ? leftindex0 : null;
+        Integer rightindex0 = getRight(L, P);
+        Integer rightindex = rightindex0 <= maxindex ? rightindex0 : null;
+
+        if (leftindex == null && rightindex == null) {
+            leaves.add(tree.get(getIndex(L, P)));
+            return;
+        }
+        if (leftindex != null) {
+            setLeaves(L + 1, 2 * P);
+        }
+        if (rightindex != null) {
+            setLeaves(L + 1, 2 * P + 1);
+        }
+
+    }
+    public void printLeaves(int L,int P){
+        marks =new ArrayList<Integer>();
+        markLeaves(L, P);
+        printTree0(tree);
+        marks=null;
+    }
+    public void printLeaves0(int L,int P){
+        markleaves = new ArrayList<Node>(tree);
+//        Collections.copy(markleaves,tree);
+        markLeaves(L,P);
+        printTree0(markleaves);
+    }
+    public void printTree(){
+        printTree0(tree);
+    }
+
+    private void printTree0(List<Node> tree) {
+        if (tree.size() == 0) {
+            System.out.println("二叉树为空！");
+            return;
+        }
+        int flag = tree.get(0).L;
+        for (Node node : tree) {
+            if(node.L>flag){
+                System.out.println("\n");
+                flag++;
+            }
+            if(marks!=null&&marks.contains(tree.indexOf(node))){
+                System.out.print("["+node.L+node.P+":"+node.V+"]");
+                continue;
+            }
+            System.out.print(node.value+"\t");
+        }
+        System.out.println();
+    }
+
+    class Node {
+        int L;
+        int P;
+        int V;
+        String value;
+
+        Node(int L, int P, int V) {
+            this.L = L;
+            this.P = P;
+            this.V = V;
+            this.value = String.valueOf(""+L +P +":" + V);
+        }
+
+        void markLeaf(){
+            this.value = String.valueOf("["+L+P+":"+V+"]");
+        }
+        @Override
+        public String toString() {
+            return "Node [L=" + L + ", P=" + P + ", V=" + V + ", value=" + value + "]";
+        }
+    }
 }
---
+++ src/main/java/alibaba/day_20170313/Test.java
@@ -0,0 +1,144 @@
+package alibaba.day_20170313;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @author YiJie  2017/7/11
+ **/
+public class Test {
+    //    @org.junit.Test
+    public static void main(String[] args) {
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//
+//        OutputStream outputStream = new BufferedOutputStream(System.out);
+//        int num = 0;
+//        while (true) {
+//
+//            try {
+//                num = Integer.parseInt(in.readLine());
+//
+//                System.out.println("*""*");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public void test() {
+//        int[] n = {1, 2, 3, 4, 5, 6, 7,8,9,1,2,3,4,5,6,8,9,1,0,2};
+
+        Scanner in = new Scanner(System.in);
+//        System.out.println("输入数值以空格分隔：");
+//        while (true) {
+//            String i = in.next();
+//            if (i.equals("#")) break;
+//            num.add(Integer.valueOf(i));
+//        }
+
+        System.out.println("请输入结点个数：");
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        int size = in.nextInt();
+        int[] tmp = new int[size];
+        for (int i = 0; i < size; i++) {
+            tmp[i] = i;
+        }
+
+        BinaryTreePath binaryTreePath = new BinaryTreePath(tmp);
+        System.out.println("--------- 打印二叉树，数据表示形式为 [列标][行标]:[数值] ---------");
+        binaryTreePath.printTree();
+        while (true) {
+            System.out.println("请输入想查询的根结点下标：");
+            int i = in.nextInt();
+            if (i == 0) break;
+//        System.out.println("\n"+binaryTreePath.getLeaves(2, 0));
+            System.out.println("--------- 打印二叉树，该根节点的叶子结点用[]标识 ---------");
+            binaryTreePath.printLeaves(i / 10, i % 10);
+        }
+        binaryTreePath.printTree();
+
+
+
+        /*
+        开3个线程，
+        一个添加/\，标记是否换行，标记是左节点还是右节点
+        一个移动根节点，每次加"  "，标记现在操作的叶子结点向上寻根的每个结点的index，用map存
+        一个读取控制台输入数据
+
+         */
+        StringBuffer path = new StringBuffer();
+        path.append("           1          \n" +
+                "       /        \\       \n" +
+                "     3           2       \n" +
+                "   /   \\       /   \\   \n" +
+                "  2     3     2     2     \n" +
+                " / \\   / \\   /   \n" +
+                "2   3 2   2 2    \n");
+        path.append(
+                "\t\t\t\t1         \n\n" +
+                        "\t\t\t3\t\t\t2       \n\n" +
+                        "\t\t2\t\t3\t2\t2\n" +
+                        "\t  /\t  \\\t\t3\t2\t2\n" +
+                        "\t2\t\t3\t2\t2\t2    \n\n");
+        String left = "/";
+        String right = "\\";
+//        String append
+//        System.out.println(path);
+
+    }
+
+
+}
+
+class MoveRoot implements Runnable {
+
+
+    Map<Integer, String> roots = new HashMap<Integer, String>();
+
+    public void setRootMap(int L, int P, int index) {
+        for (int i = 0; i < L; i++) {
+            if (P % 2 == 0) {//能整除2为左节点
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class PrintLimb implements Runnable {
+
+    TYPE nextPositionType;
+
+    public void setLastPositionType(TYPE type) {
+        nextPositionType = type;
+    }
+
+    @Override
+    public void run() {
+        switch (nextPositionType) {
+            case LEFT:
+                System.out.print("   /");
+                break;
+            case LINEEND:
+                System.out.print(" \\");
+                break;
+            case RIGHT:
+                System.out.print("\n/");
+                break;
+        }
+        //TODO 唤醒线程
+    }
+
+    enum TYPE {
+        LEFT,
+        RIGHT,
+        LINEEND;
+    }
+
+}
+
