2d1922f1f9483c4b673c7aa8fab8505aeba9baca
YiJie <736842589@qq.com>
2017-08-30 11:22:29 +0800

    快排实现寻找前k大的数

--- src/main/java/didi/FindKthMax.java
+++ src/main/java/didi/FindKthMax.java
@@ -17,6 +17,17 @@ public class FindKthMax {
             array.add(in.nextInt());
         }
         int k = array.remove(array.size() - 1);
+        bubbleSort(array, k);
+    }
+
+    /**
+     * 冒泡排序
+     * 时间复杂度 O(kn)
+     *
+     * @param array
+     * @param k
+     */
+    private static void bubbleSort(ArrayList<Integer> array, int k) {
         int max, tmp;
         for (int i = 0; i < array.size(); i++) {
             max = array.get(i);
@@ -34,5 +45,21 @@ public class FindKthMax {
                 return;
             }
         }
+    }
+
+    /**
+     * 利用快排的分治思想，在每趟划分出的更小区段里找，时间复杂度更低
+     * 时间复杂度 O(n)=O(n+n/2+n/4+...)
+     *
+     * @param array
+     * @param k
+     */
+    private static void partitionSort(ArrayList<Integer> array, int k) {
+        /*
+        从array[0]起，把比array[0]小的放左边，比array[0]大的放右边
+        若array[0]的位置比k大，则在0~array[0]的位置之间找第k大的数...
+        以此类推
+         */
+
     }
 }
--- src/main/java/didi/MaxSubsequenceSum.java
+++ src/main/java/didi/MaxSubsequenceSum.java
@@ -6,4 +6,16 @@ package didi;
  * @author YiJie 2017/8/28.
  */
 public class MaxSubsequenceSum {
+
+    public static int maxSubSum(int[] array) {
+        int sum = 0, max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+            if (sum > max)
+                max = sum;
+            if (sum < 0)  //如果 sum < 0, 将 sum 重新置 0
+                sum = 0;
+        }
+        return max;
+    }
 }
